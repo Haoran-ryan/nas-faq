ebd8f5b5e1de26f3ca00e897191f74802b6a6b5f
Haoran-ryan <gryan@outlook.com.au>
2025-05-05 16:50:52 +1000

    base path added

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://github.com/Haoran-ryan/nas-faq",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
